1a2ae78ceecad25425da3623a3d3eb8a186e2d1f
Eugene <eugeneftw500@gmail.com>
2023-03-08 14:49:47 +0800

    Modified PQ Add Function

--- SC2001_Project2/Eugene/main.py
+++ SC2001_Project2/Eugene/main.py
@@ -73,18 +73,14 @@ class priority_queue(object):
         self.len = len(self.pq)
 
     def add(self, vertex, weight):
-        if(len(self.pq)==0):
-           self.pq.append((vertex, weight))
-        elif(self.pq[0][1] > weight): 
-            self.pq.insert(0, (vertex, weight))
-        else: 
-            self.pq.append((vertex, weight))
+        self.pq.append((weight, vertex))
+        self.pq = sorted(self.pq, key=lambda x: x[0])
 
         self.len+=1
 
     def remove(self, vertex): 
         for v in self.pq: 
-            if v[0] == vertex: 
+            if v[1] == vertex: 
                 self.pq.remove(v)
                 self.len -= 1
 
@@ -93,6 +89,9 @@ class priority_queue(object):
 
     def printQueue(self):
         print(self.pq)
+
+
+
 
 def DijkstraAlgo_A(graph, source):
     d = []
@@ -114,7 +113,7 @@ def DijkstraAlgo_A(graph, source):
     while pq.len:
         
         # Get minimum weight    
-        u = pq.getMin()[0] 
+        u = pq.getMin()[1] 
         S[u] = 1
         pq.remove(u)
 
@@ -131,48 +130,87 @@ def DijkstraAlgo_A(graph, source):
             
 
 def DijkstraAlgo_B(graph, source):
-    import heapq as heap
+    # use the adjacency list to find the shortest path
     d = []
     pi = []
     S = []
-    pq = priority_queue()
+    pq = priority_queue_heap()
 
-    for v in range(graph.getSize()):
+    for v in range(graph.getSize()): # Time Complexity = O(V)
         d.append(float('inf'))
         pi.append(0)
         S.append(None)
 
     d[source-1] = 0
 
-    # Minimizing Heap Priority Queue
-    min_heap_pq = [] 
+    # Push every vertex into priority queue using array based on d[]
     for vertex in range(len(d)):
-        heap.heappush(min_heap_pq, (vertex+1, d[vertex]))
+        pq.add(vertex, d[vertex])
 
-    while len(min_heap_pq):
-        
-        # Get minimum weight    
-        u = min_heap_pq[0][0]
-        S[u] = 1
-        heap.heappop(min_heap_pq)
+    while pq.len:
+            
+            # Get minimum weight    
+            u = pq.getMin()[1] # Time Complexity = O(1)
+            S[u] = 1
+            pq.remove(u) # Time Complexity = O(V)
 
-        # For every adjacent node
-        # Node Structure: Header Node -> [(Node, Edge Weight)]
-        for nodes in graph.adjList: 
-            for node in graph.adjList[nodes]:
-                v = node[0] - 1 
-                if d[v] > d[u] + node[1]:
-                    # Remove vertex from heap
-                    min_heap_pq.remove((v+1, d[v]))
-                    heap.heapify(min_heap_pq)
+            # For every adjacent node
+            # Use BFS to traverse the graph using the adjacency list
+            for v in graph.adjList[u]: # Time Complexity = O(|E| + |V|)
+                if ((v[1] != 0) and (v[1] != float('inf'))
+                and (d[v[0]-1] > d[u] + v[1]) and (S[v[0]-1] == None)):
+                    pq.remove(v[0]-1) # Time Complexity = O(V)
+                    d[v[0]-1] = d[u] + v[1]
+                    pi[v[0]-1] = u+1
+                    pq.add(v[0]-1, d[v[0]-1]) # Time Complexity = O(log(V))
 
-                    d[v] = d[u] + node[1]
-                    pi[v] = u+1
-
-                    # Re-add vertex
-                    heap.heappush(min_heap_pq, (v+1, d[v]))
 
     return S, d, pi
+
+
+# def DijkstraAlgo_B(graph, source):
+#     import heapq as heap
+#     d = []
+#     pi = []
+#     S = []
+#     pq = priority_queue()
+
+#     for v in range(graph.getSize()):
+#         d.append(float('inf'))
+#         pi.append(0)
+#         S.append(None)
+
+#     d[source-1] = 0
+
+#     # Minimizing Heap Priority Queue
+#     min_heap_pq = [] 
+#     for vertex in range(len(d)):
+#         heap.heappush(min_heap_pq, (vertex+1, d[vertex]))
+
+#     while len(min_heap_pq):
+        
+#         # Get minimum weight    
+#         u = min_heap_pq[0][0]
+#         S[u] = 1
+#         heap.heappop(min_heap_pq)
+
+#         # For every adjacent node
+#         # Node Structure: Header Node -> [(Node, Edge Weight)]
+#         for nodes in graph.adjList: 
+#             for node in graph.adjList[nodes]:
+#                 v = node[0] - 1 
+#                 if d[v] > d[u] + node[1]:
+#                     # Remove vertex from heap
+#                     min_heap_pq.remove((v+1, d[v]))
+#                     heap.heapify(min_heap_pq)
+
+#                     d[v] = d[u] + node[1]
+#                     pi[v] = u+1
+
+#                     # Re-add vertex
+#                     heap.heappush(min_heap_pq, (v+1, d[v]))
+
+#     return S, d, pi
 
 
 def main():
